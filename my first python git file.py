3210f7a2ef0e39bbfeeae350e0dd44cf1614c4be
valkiriarmy <114300888+valkiriarmy@users.noreply.github.com>
2022-09-24 17:25:12 +0200

    Add files via upload

---
+++ my first python git file.py
@@ -0,0 +1,5 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Create a new python file
+>>> print("My first git file")
+My first git file
